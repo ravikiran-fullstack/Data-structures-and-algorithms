1e1f1db5a03ab9e46af28b031af83d1dfa7c2218
Ravikira Yadava <ravikiranjobs21@gmail.com>
2023-06-11 21:10:35 -0400

    basic sort: bubbleSort

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+console.log('BubbleSort');
+
+const sort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j <= array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+};
+
+console.log(sort([1, 6, 7, 4, 3, 2]));
